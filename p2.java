7d2032574aa2ab0c2211eb1686081d34237e1f0f
Shyam200707 <Shyamok2007@gmail.com>
2026-09-13 22:32:23 +0530

    Add number classification program in p2.java

---
+++ p2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Positive_Negative_Zero_Classifier {
+
+    static void classifyNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        classifyNumber(number);
+    }
+}
